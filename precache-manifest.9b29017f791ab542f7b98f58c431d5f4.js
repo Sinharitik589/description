c735f57a169e3e6da0419adb38d7d79c32e4b17e
Sinharitik589 <sinharitik18112835@gmail.com>
2020-07-19 15:30:49 +0530

    Updates

---
+++ precache-manifest.9b29017f791ab542f7b98f58c431d5f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b6a4870947623fdc239d9607ab8b57",
+    "url": "/description/index.html"
+  },
+  {
+    "revision": "ba8a96fc1fa0c9b4b493",
+    "url": "/description/static/css/main.150db805.chunk.css"
+  },
+  {
+    "revision": "89f0782e03bcdc4a6fff",
+    "url": "/description/static/js/2.fe4c7706.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/description/static/js/2.fe4c7706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8a96fc1fa0c9b4b493",
+    "url": "/description/static/js/main.c3e8a14d.chunk.js"
+  },
+  {
+    "revision": "63e46c577d19a72bef53",
+    "url": "/description/static/js/runtime-main.840963d5.js"
+  }
+]);
